12e3914c9d485816e5c08f0a1211e00315d24a58
Dima Tisnek <dimaqq@gmail.com>
2019-07-07 10:36:54 +0900

    Tokyo session import (#1)

---
+++ 20190704-potter-testing-library/README.md
@@ -0,0 +1,13 @@
+### Testing Library vs. Potter Kata
+
+#### Problem statement
+
+http://codingdojo.org/kata/Potter/
+
+#### Run live
+
+`npm start`
+
+#### Test
+
+`npm test`
---
+++ 20190704-potter-testing-library/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Page from "./Page"; 
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Page/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
