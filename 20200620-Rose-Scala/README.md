af94b95ab005c2cdef2d11f90dd7e2bc16505d2e
Slawek Rogulski <slawek.rogulski@gmail.com>
2020-06-22 13:32:28 +0800

    Guilded Rose Scala solution including Conjured enhancement

--- 20200620-Rose-Scala/README.md
+++ 20200620-Rose-Scala/README.md
@@ -1,5 +1,11 @@
-test command
+baseline test command
 
 ```
 diff <(sbt --error "test:run 16") baseline.text && echo OK
+```
+
+test command including conjured implementation
+
+```
+diff <(sbt --error "test:run 16") baselineAndConjured.text && echo OK
 ```
--- 20200620-Rose-Scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ 20200620-Rose-Scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -8,15 +8,18 @@ class GildedRose(val items: Array[Item]) {
       case i@Item("Sulfuras, Hand of Ragnaros")                => i
       case i@Item("Aged Brie")                                 => i.ageItem().incQuality().incQuality(_.expired)
       case i@Item("Backstage passes to a TAFKAL80ETC concert") => i.ageItem().incQuality().incQuality(_.sellIn < 10).incQuality(_.sellIn < 5).zeroQuality(_.expired)
-      case i                                                   => i.ageItem().decQuality().decQuality(_.expired)
+      case i@Item("Conjured Mana Cake")                        => handleNormalItem(i, 2)
+      case i                                                   => handleNormalItem(i, 1)
     }
   }
+
+  private def handleNormalItem(i: Item, decayRate: Int): Item = i.ageItem().decQuality(decayRate)().decQuality(decayRate)(_.expired)
 }
 
 object GildedRose {
   implicit class ItemOps(val item: Item) extends AnyVal {
-    def decQuality(guard: Item => Boolean = _ => true):Item = {
-      if (item.quality > 0 && guard(item)) item.quality -= 1
+    def decQuality(rate: Int)(guard: Item => Boolean = _ => true):Item = {
+      if (item.quality > 0 && guard(item)) item.quality -= rate
       item
     }
     def incQuality(guard: Item => Boolean = _ => true):Item = {
