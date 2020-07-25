0aa10c053051ba2982ddda207cd8c4653184b445
Slawek Rogulski <slawek.rogulski@gmail.com>
2020-07-25 18:09:55 +0800

    move order creation into purchase request

--- 20200711-tell-dont-ask-Scala/src/main/scala/orders/features/creation/OrderFactory.scala
+++ 20200711-tell-dont-ask-Scala/src/main/scala/orders/features/creation/OrderFactory.scala
@@ -1,23 +1,9 @@
 package orders.features.creation
 
-import orders.domain.Order
-import orders.domain.Order.OrderItems
-import orders.features.creation.PurchaseRequest.PurchaseRequestItems
 import orders.repository.OrderRepository.OrderId
 import orders.repository.{OrderRepository, ProductCatalogue}
 
 class OrderFactory(productCatalogue: ProductCatalogue, orderRepository: OrderRepository) {
-  def make(purchaseRequest: PurchaseRequest): Either[String, OrderId] = {
-    @scala.annotation.tailrec
-    def getOrderItems(items: PurchaseRequestItems, orderItems: OrderItems = Nil): Either[String, OrderItems] =
-      items match {
-        case Nil     => if (orderItems.isEmpty) Left("No items requested") else Right(orderItems)
-        case i +: is => productCatalogue.getByName(i.productName) match {
-          case Right(product) => getOrderItems(is, orderItems :+ Order.Item(product, i.quantity))
-          case Left(left) => Left(left)
-        }
-      }
-    import purchaseRequest._
-    getOrderItems(items) map Order.mkNew(currency) map orderRepository.addOrder
-  }
+  def make(purchaseRequest: PurchaseRequest): Either[String, OrderId] =
+    purchaseRequest.createOrder(productCatalogue) map orderRepository.addOrder
 }
